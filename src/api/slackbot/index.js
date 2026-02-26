48d61dd5bca2f83754df7fb03448d4911719cb8b
Javea Guiri <javeaguiri@gmail.com>
2026-02-26 12:01:14 +0100

    debug: Changed test code to ESM

--- src/api/slackbot/index.js
+++ src/api/slackbot/index.js
@@ -1,12 +1,12 @@
-const { App } = require('@slack/bolt');
+import { App } from '@slack/bolt';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.command("/ping", async ({ command, ack, client }) => {
+app.command("/ping", async ({ command, ack }) => {
   await ack(":wave: pong");
 });
 
-module.exports.app = app;
+export default app;
